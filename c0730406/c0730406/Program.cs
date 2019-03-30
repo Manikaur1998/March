89398d8297f8eb56de8fa97204deebec0f46811a
Manpreet Kaur <730406@student.tor.loc>
2019-03-29 20:24:24 -0400

    gdhddh

---
+++ c0730406/c0730406/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0730406
+{
+    class Program
+    {
+
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                HelloWorld hw = new HelloWorld();
+
+                hw.Hello();
+
+            }
+
+        }
+
+        class HelloWorld
+
+        {
+
+            public void Hello()
+
+            {
+
+                Console.WriteLine("Hello World");
+
+            }
+
+
+
+            public int Add(int a, int b) { return a + b; }
+
+        }
+
+    }
+
